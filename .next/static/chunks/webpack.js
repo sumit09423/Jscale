cb59712e48d3a0ee32a3912a39b01a5bc03668aa
Sumit Chauhan <85306802+sumit09423@users.noreply.github.com>
2026-06-24 22:25:35 +0530

    Update Next.js build artifacts and cache files, including changes to trace data and webpack configurations. Add new binary files for client and server development caches, and update CSS module exports for various components.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bc611412fffab461"; }
+/******/ 		__webpack_require__.h = function() { return "9cee26fad94d4d52"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
